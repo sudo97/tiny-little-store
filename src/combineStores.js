4f3080373ef214296a92e37f35551a96cf8aeccf
sudo97 <i.mihnevich@gmail.com>
2020-02-27 22:35:10 +0200

    DRY > WET

--- src/combineStores.js
+++ src/combineStores.js
@@ -1,24 +1,14 @@
+import createStore from './createStore';
+
 export default obj => {
-  let listeners = [];
+  const store = createStore({});
+  const { subscribe, getState, updateStore } = store;
 
-  const state = { ...obj };
-
-  Object.entries(state).forEach(([key, value]) => {
+  Object.entries(obj).forEach(([key, value]) => {
     value.subscribe(x => {
-      state[key] = x;
-      listeners.forEach(l => l({ ...state }));
+      updateStore({ [key]: x });
     });
   });
-
-  const subscribe = (l) => {
-    listeners = [...listeners, l];
-    l({ ...state });
-    return () => {
-      listeners = listeners.filter(_l => _l !== l);
-    };
-  };
-
-  const getState = () => ({ ...state });
 
   return {
     subscribe, getState,
